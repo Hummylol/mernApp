81424b7ef4b95476ab2df9be4860e5ba78090060
Hummylol <humaidsadath2044@gmail.com>
2024-07-19 19:55:25 -0500

    1.2

--- frontend/src/components/Addwork/AddedSuccessfully.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddedSuccessfully = () => {
-  return (
-    <div>AddedSuccessfully</div>
-  )
-}
-
-export default AddedSuccessfully
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar2 from '../Navbar/Navbar2';
+import {motion} from 'framer-motion'
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -9,22 +10,11 @@ const HomePage = () => {
     
     <div className="homepage-container">
       <Navbar2 />
-      <header className="homepage-header">
-        <h1>Welcome to Hummy's Workout App</h1>
-        <p>Your journey to a healthier and stronger you starts here!</p>
-      </header>
-      <section className="motivation-section">
-        <h2>Stay Motivated</h2>
-        <p>“The only bad workout is the one that didn’t happen.”</p>
-      </section>
-      <section className="navigation-section">
-        <h2>Get Started</h2>
         <div className="buttons">
           <button onClick={() => navigate('/workouts')}>Workouts</button>
           <button onClick={() => navigate('/progress')}>Track Progress</button>
           <button onClick={() => navigate('/profile')}>Your Profile</button>
         </div>
-      </section>
     </div>
   );
 };
